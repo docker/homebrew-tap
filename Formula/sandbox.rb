7f51e1da7a36554cd3ae568cf9add5991a8c1d34
chaizeg <42580324+chaizeg@users.noreply.github.com>
2026-02-17 14:53:07 +0000

    chore(formula): update sandbox to v0.12.0

--- Formula/sandbox.rb
+++ Formula/sandbox.rb
@@ -1,13 +1,13 @@
 class Sandbox < Formula
   desc "Docker Sandbox CLI"
   homepage "https://github.com/docker/sandboxes-releases"
-  version "0.12.0"
+  version "0.12.0-test949"
   license :cannot_represent
 
   on_macos do
     on_arm do
       url "https://github.com/docker/sandboxes-releases/releases/download/v#{version}/docker-sandbox-darwin-arm64.gz"
-      sha256 "b37bc991161734b99205a61fd099c465f811d76f2484226a4896b49f24b5266c"
+      sha256 "b37bc991161734b99205a61fd099c465f811d76f2484226a4896b49f24b5266c-test949"
     end
   end
 
