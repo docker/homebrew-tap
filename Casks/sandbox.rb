83e1a53ba653c14579d6eeaff6865699f53b1cf0
maxcleme <maxime.clement@docker.com>
2026-02-26 17:02:25 +0100

    fix(sandbox): use latest artifact structure
    Signed-off-by: maxcleme <maxime.clement@docker.com>

--- Casks/sandbox.rb
+++ Casks/sandbox.rb
@@ -2,22 +2,22 @@ cask "sandbox" do
   version "0.13.0"
   sha256 "262bceea7fea87f632b6cd4b7ffc0dc5c3c830aaa284ad40cfcd444dd7010564"
 
-  url "https://github.com/docker/sandboxes-releases/releases/download/v#{version}/docker-sandbox-darwin-arm64.gz"
+  url "https://github.com/docker/sandboxes-releases/releases/download/v#{version}/docker-sandbox-darwin-arm64.tar.gz"
   name "Docker Sandbox CLI"
   desc "Docker Sandbox CLI"
   homepage "https://github.com/docker/sandboxes-releases"
 
   depends_on cask: "docker/tap/secrets-engine"
 
-  binary "docker-sandbox-darwin-arm64", target: "sandbox"
+  binary "bin/sandbox", target: "sandbox"
 
   # Stop any running daemon before starting again
   # (Would fail on first install since binary is not yet in place)
   preflight do
     system_command "/bin/chmod",
-                   args:         ["+x", "#{staged_path}/docker-sandbox-darwin-arm64"],
+                   args:         ["+x", "#{staged_path}/bin/sandbox"],
                    must_succeed: true
-    system_command "#{staged_path}/docker-sandbox-darwin-arm64",
+    system_command "#{staged_path}/bin/sandbox",
                    args:         ["daemon", "stop"],
                    must_succeed: false
   end
