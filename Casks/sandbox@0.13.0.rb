f6e39ec0f12054addf20a29692f87b4742eec3a4
maxcleme <maxime.clement@docker.com>
2026-02-26 16:54:57 +0100

    Revert "fix(sandbox): use latest artifact structure"
    This reverts commit 88e3190dd51636d06c6f14a9d22e0c0eae3aaec8.

--- Casks/sandbox@0.13.0.rb
+++ Casks/sandbox@0.13.0.rb
@@ -2,22 +2,22 @@ cask "sandbox@0.13.0" do
   version "0.13.0"
   sha256 "1ba18aaf923898702c2bf3f9c83bdb20109361c15289ab0348150d6240c9a8f4"
 
-  url "https://github.com/docker/sandboxes-releases/releases/download/v#{version}/docker-sandbox-darwin-arm64.tar.gz"
+  url "https://github.com/docker/sandboxes-releases/releases/download/v#{version}/docker-sandbox-darwin-arm64.gz"
   name "Docker Sandbox CLI"
   desc "Docker Sandbox CLI"
   homepage "https://github.com/docker/sandboxes-releases"
 
   depends_on cask: "docker/tap/secrets-engine"
 
-  binary "bin/sandbox", target: "sandbox"
+  binary "docker-sandbox-darwin-arm64", target: "sandbox"
 
   # Stop any running daemon before starting again
   # (Would fail on first install since binary is not yet in place)
   preflight do
     system_command "/bin/chmod",
-                   args:         ["+x", "#{staged_path}/bin/sandbox"],
+                   args:         ["+x", "#{staged_path}/docker-sandbox-darwin-arm64"],
                    must_succeed: true
-    system_command "#{staged_path}/bin/sandbox",
+    system_command "#{staged_path}/docker-sandbox-darwin-arm64",
                    args:         ["daemon", "stop"],
                    must_succeed: false
   end
