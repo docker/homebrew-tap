a3eae24d2b399524c052c0e9e6530bb0e2d66c69
maxcleme <maxime.clement@docker.com>
2026-03-05 11:14:14 +0100

    fix(sandbox): use latest artifact structure
    Signed-off-by: maxcleme <maxime.clement@docker.com>

--- Casks/ds@0.13.0.rb
+++ Casks/ds@0.13.0.rb
@@ -3,9 +3,9 @@ require strategy_path if File.exist?("#{strategy_path}.rb")
 
 cask "ds@0.13.0" do
   version "0.13.0"
-  sha256 "68690e8ad755d22dc2983ca62c5bab47e11166eeb73d75e048645ba1e420d1d7"
+  sha256 "819dec0def59236ce4df23f1e455baac33f4ba30819a72b9e890d93233e86301"
 
-  url "https://github.com/docker/dash-releases/releases/download/v#{version}/ds-darwin-arm64.tar.gz",
+  url "https://github.com/docker/dash-releases/releases/download/v#{version}/Dash.dmg",
       using: GitHubPrivateRepositoryReleaseDownloadStrategy
   name "Docker Dash CLI"
   desc "Docker Dash CLI"
@@ -13,15 +13,15 @@ cask "ds@0.13.0" do
 
   depends_on cask: "docker/tap/secrets-engine"
 
-  binary "bin/ds", target: "ds@0.13.0"
+  binary "Dash.app/Contents/MacOS/ds", target: "ds@0.13.0"
 
   # Stop any running daemon before starting again
   # (Would fail on first install since binary is not yet in place)
   preflight do
     system_command "/bin/chmod",
-                   args:         ["+x", "#{staged_path}/bin/ds"],
+                   args:         ["+x", "#{staged_path}/Dash.app/Contents/MacOS/ds"],
                    must_succeed: true
-    system_command "#{staged_path}/bin/ds",
+    system_command "#{staged_path}/Dash.app/Contents/MacOS/ds",
                    args:         ["daemon", "stop"],
                    must_succeed: false
   end
