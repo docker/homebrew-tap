9482a99a0f0c0e95ba09762faa4c99f6f57b37b7
George Gabolaev <george.gabolaev@docker.com>
2026-03-06 12:01:23 +0100

    Revert "chore(cask): update ds to nightly"
    This reverts commit 0bd70fbbaef6aa6265cd6a12bbd8b8c33f813dc5.

--- Casks/ds.rb
+++ Casks/ds.rb
@@ -2,21 +2,18 @@ strategy_path = File.expand_path("../custom_download_strategy", __dir__)
 require strategy_path if File.exist?("#{strategy_path}.rb")
 
 cask "ds" do
-  version "nightly"
-  sha256 "26fd3e474f7f79b7b4344957ec72589b2fbef529ab2c44dad128bc72066a31a6"
+  version "0.13.0"
+  sha256 "60847835245aafce7d280df177d3ef2711e1fba396dafc5b190e77b612bc2431"
 
   url "https://github.com/docker/dash-releases/releases/download/v#{version}/Dash.dmg",
       using: GitHubPrivateRepositoryReleaseDownloadStrategy
-  name "Dash by Docker"
-  desc "Build, run, and govern agents across the software development lifecycle."
+  name "Docker Dash CLI"
+  desc "Docker Dash CLI"
   homepage "https://github.com/docker/dash-releases"
 
   depends_on cask:  "docker/tap/secrets-engine",
              arch:  :arm64,
              macos: ">= :tahoe"
 
-  binary "bin/ds", target: "ds"
-  bash_completion "etc/bash_completion.d/ds"
-  zsh_completion "share/zsh/site-functions/_ds"
-  fish_completion "share/fish/vendor_completions.d/ds.fish"
+  binary "Dash.app/Contents/MacOS/ds", target: "ds"
 end
