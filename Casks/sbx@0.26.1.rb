1b759042462bdbb96ecca8e0f76627325d749f02
Dorin Geman <dorin.geman@docker.com>
2026-06-10 18:27:38 +0300

    fix(casks): use symbol form for depends_on macos:
    Signed-off-by: Dorin Geman <dorin.geman@docker.com>

--- Casks/sbx@0.26.1.rb
+++ Casks/sbx@0.26.1.rb
@@ -8,7 +8,7 @@ cask "sbx@0.26.1" do
   homepage "https://github.com/docker/sbx-releases"
 
   depends_on arch:  :arm64,
-             macos: ">= :tahoe"
+             macos: :tahoe
 
   binary "bin/sbx", target: "sbx-0.26.1"
   bash_completion "completions/bash/sbx", target: "sbx-0.26.1"
